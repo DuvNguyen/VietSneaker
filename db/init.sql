dd61c10dc223f99b14b9a388e80ffaede93a6747
Nguyen Duc Vi <nducvi.information@gmail.com>
2025-09-18 19:17:30 +0700

    connect with mysql, config driver, create new keystore

--- db/init.sql
+++ db/init.sql
@@ -1,10 +1,1 @@
-IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'clockeedb')
-BEGIN
-    CREATE DATABASE ClockeeDB;
-    PRINT 'Database ClockeeDB created successfully.';
-END
-ELSE
-BEGIN
-    PRINT 'Database ClockeeDB already exists.';
-END
-GO
+CREATE DATABASE IF NOT EXISTS vietsneaker;
