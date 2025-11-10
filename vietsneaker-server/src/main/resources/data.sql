3d93f28b5852492e8af8fc6874f84ecaf4ea8d0f
Nguyen Duc Vi <nducvi.information@gmail.com>
2025-11-10 15:07:03 +0700

    change af1 picture, delete unused pictures

--- vietsneaker-server/src/main/resources/data.sql
+++ vietsneaker-server/src/main/resources/data.sql
@@ -67,19 +67,34 @@ INSERT INTO roles_users (user_id, role_id)
 --        ('Fila Disruptor II', 'Thiết kế hầm hố chunky', 'fila_disruptor.jpg', 2600000, 2800000, 'Streetwear', 30, 9, 1, 1, 0),
 --        ('Under Armour Curry Flow 10', 'Giày bóng rổ của Stephen Curry', 'ua_curry10.jpg', 4200000, 4500000, 'Basketball', 15, 10, 1, 1, 0);
 
+-- INSERT INTO products
+-- (name, description, image_url, actual_price, sell_price, type, shoe_size, stock, brand_id, is_active, visible, is_deleted)
+-- VALUES
+-- ('Nike Air Force 1', 'Giày sneaker huyền thoại của Nike, condition khá tốt, vân đế gần full', 'nike_airforce1.jpg', 700000, 1200000, 'Casual', '42', 50, 1, 1, 1, 0),
+-- ('Adidas Ultraboost 22', 'Giày chạy bộ hiệu năng cao', 'adidas_ultraboost22.jpg', 4500000, 4800000, 'Running', '41', 30, 2, 1, 1, 0),
+-- ('Puma Suede Classic', 'Thiết kế retro, phong cách đường phố', 'puma_suede.jpg', 2000000, 2200000, 'Lifestyle', '40', 40, 3, 1, 1, 0),
+-- ('Converse Chuck Taylor 70s', 'Mẫu giày canvas cổ điển', 'converse_chuck70.jpg', 1800000, 2000000, 'Casual', '42', 60, 4, 1, 1, 0),
+-- ('Vans Old Skool', 'Sneaker skateboarding nổi tiếng', 'vans_oldskool.jpg', 1900000, 2100000, 'Skate', '41', 55, 5, 1, 1, 0),
+-- ('New Balance 550', 'Phong cách retro basketball', 'newbalance_550.jpg', 3200000, 3500000, 'Lifestyle', '43', 25, 6, 1, 1, 0),
+-- ('Reebok Classic Leather', 'Phong cách vintage từ thập niên 80', 'reebok_classic.jpg', 2200000, 2500000, 'Lifestyle', '42', 35, 7, 1, 1, 0),
+-- ('ASICS Gel-Kayano 29', 'Giày chạy bộ bền bỉ', 'asics_kayano.jpg', 4000000, 4300000, 'Running', '41', 20, 8, 1, 1, 0),
+-- ('Fila Disruptor II', 'Thiết kế hầm hố chunky', 'fila_disruptor.jpg', 2600000, 2800000, 'Streetwear', '40', 30, 9, 1, 1, 0),
+-- ('Under Armour Curry Flow 10', 'Giày bóng rổ của Stephen Curry', 'ua_curry10.jpg', 4200000, 4500000, 'Basketball', '44', 15, 10, 1, 1, 0);
+
 INSERT INTO products
 (name, description, image_url, actual_price, sell_price, type, shoe_size, stock, brand_id, is_active, visible, is_deleted)
 VALUES
-('Nike Air Force 1', 'Giày sneaker huyền thoại của Nike, condition khá tốt, vân đế gần full', 'nike_airforce1.jpg', 700000, 1200000, 'Casual', '42', 50, 1, 1, 1, 0),
-('Adidas Ultraboost 22', 'Giày chạy bộ hiệu năng cao', 'adidas_ultraboost22.jpg', 4500000, 4800000, 'Running', '41', 30, 2, 1, 1, 0),
-('Puma Suede Classic', 'Thiết kế retro, phong cách đường phố', 'puma_suede.jpg', 2000000, 2200000, 'Lifestyle', '40', 40, 3, 1, 1, 0),
-('Converse Chuck Taylor 70s', 'Mẫu giày canvas cổ điển', 'converse_chuck70.jpg', 1800000, 2000000, 'Casual', '42', 60, 4, 1, 1, 0),
-('Vans Old Skool', 'Sneaker skateboarding nổi tiếng', 'vans_oldskool.jpg', 1900000, 2100000, 'Skate', '41', 55, 5, 1, 1, 0),
-('New Balance 550', 'Phong cách retro basketball', 'newbalance_550.jpg', 3200000, 3500000, 'Lifestyle', '43', 25, 6, 1, 1, 0),
-('Reebok Classic Leather', 'Phong cách vintage từ thập niên 80', 'reebok_classic.jpg', 2200000, 2500000, 'Lifestyle', '42', 35, 7, 1, 1, 0),
-('ASICS Gel-Kayano 29', 'Giày chạy bộ bền bỉ', 'asics_kayano.jpg', 4000000, 4300000, 'Running', '41', 20, 8, 1, 1, 0),
-('Fila Disruptor II', 'Thiết kế hầm hố chunky', 'fila_disruptor.jpg', 2600000, 2800000, 'Streetwear', '40', 30, 9, 1, 1, 0),
-('Under Armour Curry Flow 10', 'Giày bóng rổ của Stephen Curry', 'ua_curry10.jpg', 4200000, 4500000, 'Basketball', '44', 15, 10, 1, 1, 0);
+('Nike Air Force 1', 'Giày sneaker huyền thoại của Nike – hàng 2hand chính hãng, **vân đế gần full**, form giữ chuẩn, upper đẹp như mới, chỉ vài vết sử dụng nhẹ.', 'nike_airforce1.jpg', 700000, 950000, 'Casual', '42', 50, 1, 1, 1, 0),
+('Adidas Ultraboost 22', 'Giày chạy bộ hiệu năng cao của Adidas – hàng 2hand, đế vẫn còn bám tốt, form chưa xẹp, upper còn rất đẹp.', 'adidas_ultraboost22.jpg', 4500000, 3800000, 'Running', '41', 30, 2, 1, 1, 0),
+('Puma Suede Classic', 'Thiết kế retro đường phố của Puma – hàng 2hand chính hãng, **vân đế rõ nét**, form vẫn giữ tốt, suede chỉ vài vết sử dụng nhỏ.', 'puma_suede.jpg', 2000000, 1600000, 'Lifestyle', '40', 40, 3, 1, 1, 0),
+('Converse Chuck Taylor 70s', 'Mẫu giày canvas cổ điển – hàng 2hand, form ổn, đế chưa mòn nhiều, vải và đế giữ rất tốt.', 'converse_chuck70.jpg', 1800000, 1400000, 'Casual', '42', 60, 4, 1, 1, 0),
+('Vans Old Skool', 'Sneaker skate nổi tiếng – hàng 2hand chính hãng, đế hơi ngả màu theo thời gian nhưng **form vẫn chuẩn**, upper còn đẹp.', 'vans_oldskool.jpg', 1900000, 1500000, 'Skate', '41', 55, 5, 1, 1, 0),
+('New Balance 550', 'Phong cách retro basketball của NB – hàng 2hand, mid-sole còn dày, upper chỉ hơi nhăn, form vẫn chắc.', 'newbalance_550.jpg', 3200000, 2500000, 'Lifestyle', '43', 25, 6, 1, 1, 0),
+('Reebok Classic Leather', 'Phong cách vintage từ thập niên 80 – hàng 2hand, da upper giữ tốt, chỉ có vết gấp nhỏ ở mũi, form vẫn đẹp.', 'reebok_classic.jpg', 2200000, 1700000, 'Lifestyle', '42', 35, 7, 1, 1, 0),
+('ASICS Gel-Kayano 29', 'Giày chạy bộ bền bỉ – hàng 2hand, đế chưa bị sờn, form chưa xẹp, chỉ có vài vết nhỏ, cực hợp tập hoặc đi bộ.', 'asics_kayano.jpg', 4000000, 3300000, 'Running', '41', 20, 8, 1, 1, 0),
+('Fila Disruptor II', 'Thiết kế chunky hầm hố Streetwear – hàng 2hand chính hãng, đế vẫn cao rõ, upper còn đẹp, form giữ tốt.', 'fila_disruptor.jpg', 2600000, 2000000, 'Streetwear', '40', 30, 9, 1, 1, 0),
+('Under Armour Curry Flow 10', 'Giày bóng rổ của Stephen Curry – hàng 2hand, độ mòn đế thấp, form vẫn rất đẹp, phù hợp chơi bóng hoặc diện hàng ngày.', 'ua_curry10.jpg', 4200000, 3400000, 'Basketball', '44', 15, 10, 1, 1, 0);
+
 
 -- Cart Items
 INSERT INTO cart_items (product_id, user_id, quantity)
